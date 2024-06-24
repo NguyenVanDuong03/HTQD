0c490d41c79d67b405d10ba32fe0f17cd4a90648
NguyenVanDuong03 <19.nguyenvanduong@gmail.com>
2024-06-24 14:11:11 +0700

    update thongTinThanhToan

--- js/thongTinThanhToan.js
+++ js/thongTinThanhToan.js
@@ -14,7 +14,7 @@ const thongTinThanhToan = `
                         <h5 style="font-weight: bold;" class="">SĐT:</h5>
                     </div>
                     <div class="col-8 my-2">
-                        <span class="" style="font-size: 20px;">039709972</span>
+                        <span class="" style="font-size: 20px;">0397099702</span>
                     </div>
                     <div class="col-4 my-2">
                         <h5 style="font-weight: bold;" class="">Địa chỉ:</h5>
@@ -26,7 +26,7 @@ const thongTinThanhToan = `
                         <h5 style="font-weight: bold;" class="">Số người lớn:</h5>
                     </div>
                     <div class="col-8 my-2">
-                        <span class="" style="font-size: 20px;">2</span>
+                        <span class="" style="font-size: 20px;">1</span>
                     </div>
                     <div class="col-4 my-2">
                         <h5 style="font-weight: bold;" class="">Số trẻ em:</h5>
@@ -44,19 +44,19 @@ const thongTinThanhToan = `
                         <h5 style="font-weight: bold;" class="">Ngày nhận phòng:</h5>
                     </div>
                     <div class="col-8 my-2">
-                        <span class="" style="font-size: 20px;">9h00 - 05/06/2024</span>
+                        <span class="" style="font-size: 20px;">08h00 - 27/06/2024</span>
                     </div>
                     <div class="col-4 my-2">
                         <h5 style="font-weight: bold;" class="">Ngày trả phòng:</h5>
                     </div>
                     <div class="col-8 my-2">
-                        <span class="" style="font-size: 20px;">10h00 - 07/06/2024</span>
+                        <span class="" style="font-size: 20px;">10h00 - 28/06/2024</span>
                     </div>
                     <div class="col-4 my-2">
                         <h5 style="font-weight: bold;" class="">Tổng thanh toán:</h5>
                     </div>
                     <div class="col-8 my-2">
-                        <h5 class="" style="color: #33AA29; font-weight: bold;">1.000.0000 VNĐ</h5>
+                        <h5 class="" style="color: #33AA29; font-weight: bold;">10.000.0000 VNĐ</h5>
                     </div>    
                 </div>
             </div>
